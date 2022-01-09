e9546bbb31f07617610ec304824311df7a76f6e6
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-01-09 14:10:39 -0600

    Add files via upload

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ import "C"
 import (
     "os"
     "fmt"
+    "unsafe"
 )
 
 func main() {
 
     //archivo a procesar
-    file, err := os.Open("cara.jpg")
+    file, err := os.Open("original.jpg")
     if err != nil {
         fmt.Println(err)
         return
@@ -47,15 +48,32 @@ func main() {
     error := C.bufftofile(cbuffer,csize)
 
     //mostramos resultado
-    fmt.Println("\nArchivo: OUT")
+    fmt.Println("Archivo: 'original.jpg' replicado en C")
     fmt.Println("Errores: ",error)
+	
     
     
-    //foo := C.CString("")
-    //defer C.free(unsafe.Pointer(foo))
-    //C.test(foo)
-    //fmt.Println("Output is: "+C.GoString(foo)*/
-    asciiBytes := []byte(C.GoString(cbuffer))
+
     
-    
+	//creamos el archivo
+    f, err := os.Create("bufferGO")
+    if err != nil {
+		fmt.Println("ERROR - No se pudo crear el archivo 'bufferGO'\n")
+		return
+    }
+    defer f.Close()
+
+	//de cbuffer a gobuffer
+	gobuffer := C.GoBytes(unsafe.Pointer(cbuffer), C.int(filesize))
+	_, err2 := f.Write(gobuffer)
+	if err2 != nil {
+		fmt.Println("ERROR - No se pudo escribir el archivo 'bufferGO'\n")
+		return
+	}
+	
+	//mostramos resultado
+	fmt.Println("Archivo: 'original.jpg' replicado en GO")
+	fmt.Println("Errores:  0")
+	    
 }
+
