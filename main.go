bc817011de2cc6ae352028388f11f0f3d3809762
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-01-13 15:28:36 -0600

    Update main.go

--- main.go
+++ main.go
@@ -5,15 +5,12 @@ package main
 #include "bufftofile.h" 
 */  
 import "C" 
-
 import (
     "os"
     "fmt"
     "unsafe"
 )
-
 func main() {
-
     //archivo a procesar
     file, err := os.Open("original.jpg")
     if err != nil {
@@ -21,48 +18,36 @@ func main() {
         return
     }
     defer file.Close()
-
     //archivo estructurado
     fileinfo, err := file.Stat()
     if err != nil {
         fmt.Println(err)
         return
     }
-
     //archivo estructurado a buffer
     filesize := fileinfo.Size()
     buffer := make([]byte, filesize)
-
     //leer buffer
     bytesread, err := file.Read(buffer)
     if err != nil {
         fmt.Println(err)
         return
     }
-
     //convertir buffer a buffer para C
     cbuffer := C.CBytes(buffer)
     csize := C.long(bytesread)
-
     //enviamos buffer a C
     error := C.bufftofile(cbuffer,csize)
-
     //mostramos resultado
     fmt.Println("Archivo: 'original.jpg' replicado en C")
     fmt.Println("Errores: ",error)
-	
-    
-    
-
-    
     //creamos el archivo
     f, err := os.Create("bufferGO")
     if err != nil {
         fmt.Println("ERROR - No se pudo crear el archivo 'bufferGO'\n")
         return
     }
-    defer f.Close()
-
+    defer f.Close(
     //de cbuffer a gobuffer
     gobuffer := C.GoBytes(unsafe.Pointer(cbuffer), C.int(filesize)+1)
     _, err2 := f.Write(gobuffer)
@@ -70,10 +55,8 @@ func main() {
         fmt.Println("ERROR - No se pudo escribir el archivo 'bufferGO'\n")
         return
     }
-
     //mostramos resultado
     fmt.Println("Archivo: 'original.jpg' replicado en GO")
     fmt.Println("Errores:  0")
-
 }
 
