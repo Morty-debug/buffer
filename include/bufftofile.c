61b8c33ef54a824c419a027482f00aef6848931d
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-01-13 15:29:09 -0600

    Update bufftofile.c

--- include/bufftofile.c
+++ include/bufftofile.c
@@ -1,22 +1,18 @@
 #include "bufftofile.h"
 #include <stdio.h> 
 #include <stdlib.h> 
-
 int bufftofile(void *buffer, long size){
-
 	//creamos el archivo
 	FILE *f_dst = fopen("bufferC", "wb");
 	if(f_dst == NULL){
 		printf("ERROR - No se pudo crear el archivo 'bufferC'\n");
 		return 1;
 	}
-
 	//escribimos en el archivo
 	if(fwrite(buffer, 1, size, f_dst) != size){
 		printf("ERROR - No se pudo escribir el archivo 'bufferC'\n");
 		return 2;
 	}
-
 	//cerramos el archivo
 	fclose(f_dst);
 	f_dst = NULL;
